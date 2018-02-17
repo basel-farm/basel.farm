b4bfd77f39efdfd5eee8a0a773e89577c53d7f11
Kenny <you@example.com>
2018-02-17 14:38:58 +0100

    Enable registration for customers

--- baslerbauer_site/baslerbauer_main/views/register.py
+++ baslerbauer_site/baslerbauer_main/views/register.py
@@ -2,18 +2,21 @@ from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.template import loader
 from django.views.decorators.csrf import csrf_exempt, csrf_protect
-from baslerbauer_main.models import Producer
-
+from baslerbauer_main.models import Producer,Consumer
+from django.shortcuts import redirect
 
 @csrf_exempt
 def register(request):
     @csrf_protect
     def user_register(request):
         user = User.objects.create_user(username=request.POST.get("email"),password=request.POST.get("password"));
-        p = Producer.objects.get(pk=request.POST.get("producer"))
-        p.user=user
-        p.save()
-        return HttpResponse("Registered")
+        if request.POST.get("type") == "type_producer":
+            p = Producer.objects.get(pk=request.POST.get("producer"))
+            p.user=user
+            p.save()
+        elif request.POST.get("type")=="type_consumer":
+            Consumer.objects.create(user=user)
+        return redirect("login")
     if request.method == 'POST':
         return user_register(request)
     else:
